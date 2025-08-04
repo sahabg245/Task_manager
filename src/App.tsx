d2460a96ebb7fdaa62bc4b29ca1e71e6a2ba0988
ABDUL QUDOOS <abdulqaudoos786@gmail.com>
2025-08-04 17:01:45 +0500

    final updated version

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import Modal from './components/Modal'
-import Calculator from "./components/Calculator"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-
-
-const router = createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:<Modal/>
-    },
-    {
-      path:"/calculator",
-      element:<Calculator/>
-    }
-
-  ]
-)
-
-
 
 function App() {
 
   return (
     <>
-        <RouterProvider router={router}/>
+        <Modal/>
     </>
   )
 }
